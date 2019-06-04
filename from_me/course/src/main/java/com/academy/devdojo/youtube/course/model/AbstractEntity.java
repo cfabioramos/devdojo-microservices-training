29115402aba6dc128a8a36be087e9a8234bf09a6
Carlos Fabio <cfabioramos@gmail.com>
2019-06-03 23:02:35 -0300

    Aula do dia 03-06/2019

--- from_me/course/src/main/java/com/academy/devdojo/youtube/course/model/AbstractEntity.java
+++
@@ -1,7 +0,0 @@
-package com.academy.devdojo.youtube.course.model;
-
-import java.io.Serializable;
-
-public interface AbstractEntity extends Serializable {
-    Long getId();
-}
--- from_me/course/src/main/java/com/academy/devdojo/youtube/course/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.academy.devdojo.youtube.course.repository;
-
-import com.academy.devdojo.youtube.course.model.Course;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
-
-}
